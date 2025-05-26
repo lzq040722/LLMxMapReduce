515cfc5c57cf0f63c870f30ba86ff29423aadc46
Desperate <162794787+lzq040722@users.noreply.github.com>
2025-05-26 22:10:06 +0800

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,15 +1,1 @@
-from google import genai
 
-client = genai.Client(api_key="AIzaSyC46-pzZLDSNG_ZS9ZCQH_Ad8XpIkSMd7k")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
-
-
-# AIzaSyDX1hkB7aiZjXr-dm7wUux0dun9w8_LwMI-cst
-# AIzaSyD4nCq4FYeUZwPqX4Pf4TttgikkEC1z6c8-ly
-# AIzaSyC46-pzZLDSNG_ZS9ZCQH_Ad8XpIkSMd7k-mine
-#AIzaSyBNo8P7UDowKjYllzPJSlxYbOI1fR82SsM-lzx
-#AIzaSyCcfcy3bkSeqfpNZ5ALqqQ2-RlBNrIQfyE-hunter
